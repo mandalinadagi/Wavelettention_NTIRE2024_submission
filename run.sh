e0a93cb8e81d3e180c106dca69f6648f15943af4
ckorkmaz14 <ckorkmaz14@ai20.kuacc.ku.edu.tr>
2024-03-22 22:23:19 +0300

    wavelettention added to the repo of NTIRE2024 image sr x4 challenge

---
+++ run.sh
@@ -0,0 +1,3 @@
+CUDA_VISIBLE_DEVICES=0 python test_demo.py --data_dir ./data --save_dir ./save_dir --model_id 17
+# CUDA_VISIBLE_DEVICES=0 python test_demo.py --data_dir NTIRE2023_ImageSR_x4/data --save_dir NTIRE2023_ImageSR_x4/results --model_id 0
+
